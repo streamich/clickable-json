5e26fc3cb61a3ea01f0b3a9bfca0438af19a8a70
streamich <streamich@gmail.com>
2023-11-21 22:21:44 +0100

    refactor: 💡 move <JsonValue> and <JsonProperty> into own files

---
+++ src/ClickableJson/JsonProperty.tsx
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import AutosizeInput from '../AutosizeInput';
+import {context} from './context';
+import * as css from '../css';
+import type {OnChange} from './types';
+
+export interface JsonPropertyProps {
+  pointer: string;
+  onChange?: OnChange;
+}
+
+export const JsonProperty: React.FC<JsonPropertyProps> = ({pointer, onChange}) => {
+  const {formal: selectable} = React.useContext(context);
+  const steps = React.useMemo(() => pointer.split('/'), [pointer]);
+  const property = React.useMemo(() => steps[steps.length - 1], [steps]);
+  const [proposed, setProposed] = React.useState(property);
+  const inputRef = React.useRef<HTMLInputElement>(null);
+  const [focused, setFocused] = React.useState(false);
+
+  const onSubmit = (e: React.FormEvent) => {
+    if (e) e.preventDefault();
+    if (e) e.stopPropagation();
+    if (onChange)
+      onChange([{op: 'move', from: pointer, path: steps.slice(0, steps.length - 1).join('/') + '/' + proposed}]);
+  };
+
+  return (
+    <>
+      {!onChange ? (
+        <span className={css.property}>{selectable ? JSON.stringify(property) : property}</span>
+      ) : (
+        <AutosizeInput
+          inputRef={(el) => ((inputRef as any).current = el)}
+          inputClassName={css.property + css.input}
+          value={focused ? proposed : property}
+          onChange={(e) => setProposed(e.target.value)}
+          onFocus={() => setFocused(true)}
+          onBlur={(e) => {
+            setFocused(false);
+            onSubmit(e);
+          }}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              if (inputRef.current) inputRef.current.blur();
+            } else if (e.key === 'Escape') {
+              if (inputRef.current) inputRef.current.blur();
+            }
+          }}
+        />
+      )}
+      <span className={css.colon}>
+        <span>:</span>
+      </span>
+    </>
+  );
+};
---
+++ src/ClickableJson/JsonValue.tsx
@@ -0,0 +1,89 @@
+import * as React from 'react';
+import AutosizeInput from '../AutosizeInput';
+import * as css from '../css';
+import {JsonProperty} from './JsonProperty';
+import type {OnChange} from './types';
+
+export interface JsonValueProps {
+  pointer: string;
+  property?: string | number;
+  doc: unknown;
+  comma?: boolean;
+  onChange?: OnChange;
+}
+
+export const JsonValue: React.FC<JsonValueProps> = (props) => {
+  const {pointer, property, doc, comma, onChange} = props;
+  const inputRef = React.useRef<HTMLInputElement>(null);
+  const className = React.useMemo(
+    () =>
+      doc === null
+        ? css.nil
+        : typeof doc === 'boolean'
+          ? css.bool
+          : typeof doc === 'string'
+            ? css.str
+            : doc === Math.round(Number(doc))
+              ? css.num
+              : css.float,
+    [doc],
+  );
+  const value = React.useMemo(
+    () =>
+      doc === null
+        ? 'null'
+        : typeof doc === 'boolean'
+          ? doc
+            ? 'true'
+            : 'false'
+          : typeof doc === 'string'
+            ? JSON.stringify(doc)
+            : String(doc),
+    [doc],
+  );
+  const [proposed, setProposed] = React.useState(value);
+  const [focused, setFocused] = React.useState(false);
+  React.useEffect(() => {
+    setProposed(value);
+  }, [value]);
+
+  const onSubmit = (e: React.KeyboardEvent) => {
+    if (e) e.preventDefault();
+    if (e) e.stopPropagation();
+    let newValue;
+    try {
+      newValue = JSON.parse(proposed);
+    } catch {
+      newValue = String(proposed);
+    }
+    if (onChange) onChange([{op: 'replace', path: pointer, value: newValue}]);
+  };
+
+  return (
+    <>
+      {typeof property === 'string' && <JsonProperty pointer={pointer} onChange={onChange} />}
+      {!onChange ? (
+        <span className={className}>{value}</span>
+      ) : (
+        <AutosizeInput
+          inputRef={(el) => ((inputRef as any).current = el)}
+          inputClassName={className + css.input}
+          value={focused ? proposed : value}
+          onChange={(e) => setProposed(e.target.value)}
+          onFocus={() => setFocused(true)}
+          onBlur={() => setFocused(false)}
+          onKeyDown={(e: React.KeyboardEvent) => {
+            if (e.key === 'Enter') {
+              if (inputRef.current) inputRef.current.blur();
+              onSubmit(e);
+            } else if (e.key === 'Escape') {
+              if (value !== proposed) setProposed(value);
+              else if (inputRef.current) inputRef.current.blur();
+            }
+          }}
+        />
+      )}
+      {!!comma && ','}
+    </>
+  );
+};
