0889440cfcd8c13fec10c701ff399b943e3e1df4
streamich <streamich@gmail.com>
2023-11-26 18:11:13 +0100

    refactor: 💡 separate out new value creation logic

--- src/ClickableJsonCrdt/nodes/JsonCrdtObjNode/AddKey.tsx
+++ src/ClickableJsonCrdt/nodes/JsonCrdtObjNode/AddKey.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import {NodeRef} from '../../NodeRef';
 import {ObjectInsert} from '../../../inserts/ObjectInsert';
 import {useIsFocused} from '../../../context/focus';
-import {id} from '../../utils';
+import {createValue, id} from '../../utils';
 import {useJsonCrdt} from '../../context';
 import {CrdtTypeSwitch} from '../../../buttons/CrdtTypeSwitch';
 import type {ObjNode} from 'json-joy/es2020/json-crdt';
@@ -14,36 +14,13 @@ export interface AddKeyProps {
 export const AddKey: React.FC<AddKeyProps> = ({node}) => {
   const {model} = useJsonCrdt();
   const isFocused = useIsFocused(id(node));
-  const type = React.useRef<string>('any');
+  const type = React.useRef<'any' | 'con' | 'vec' | 'val'>('any');
 
   const handleSubmit = React.useCallback(
     (key: string, json: string) => {
-      let value: unknown = json;
-      try {
-        value = JSON.parse(json);
-      } catch {}
-      const api = model.api;
-      const nodeApi = api.wrap(node.node);
-      nodeApi.set({
-        [key]:
-          type.current === 'any'
-            ? value
-            : type.current === 'con'
-              ? api.builder.const(value)
-              : type.current === 'vec'
-                ? api.builder.vec()
-                : type.current === 'val'
-                  ? api.builder.val()
-                  : api.builder.const(undefined),
-      });
-      if (type.current === 'vec') {
-        if (json) {
-          const valueVec = Array.isArray(value) ? value : [value];
-          nodeApi.tup(key).set(valueVec.map((x, i) => [i, api.builder.maybeConst(x)]));
-        }
-      } else if (type.current === 'val') {
-        nodeApi.val(key).set(json ? api.builder.maybeConst(value) : api.builder.const(undefined));
-      }
+      const valueId = createValue(model, json, type.current);
+      const nodeApi = model.api.wrap(node.node);
+      nodeApi.set({[key]: valueId});
     },
     [node.node, type],
   );
--- src/ClickableJsonCrdt/utils.ts
+++ src/ClickableJsonCrdt/utils.ts
@@ -1,7 +1,35 @@
 import {NodeRef} from './NodeRef';
-import type {JsonNode} from 'json-joy/es2020/json-crdt';
+import type {JsonNode, Model} from 'json-joy/es2020/json-crdt';
+import type {ITimestampStruct} from 'json-joy/es2020/json-crdt-patch/clock';
 
 export const id = (node: NodeRef<JsonNode>) => {
   const id = node.node.id;
   return id.sid + '.' + id.time;
+};
+
+export const createValue = (model: Model, json: string, type: 'any' | 'con' | 'vec' | 'val'): ITimestampStruct => {
+  let value: unknown = json;
+  try {
+    value = JSON.parse(json);
+  } catch {}
+  const api = model.api;
+  const builder = api.builder;
+  const valueId = type === 'any'
+    ? builder.json(value)
+    : type === 'con'
+      ? api.builder.const(value)
+      : type === 'vec'
+        ? api.builder.vec()
+        : type === 'val'
+          ? api.builder.val()
+          : api.builder.const(undefined);
+  if (type === 'vec') {
+    if (json) {
+      const valueVec = Array.isArray(value) ? value : [value];
+      builder.insVec(valueId, valueVec.map((x, i) => [i, api.builder.maybeConst(x)]));
+    }
+  } else if (type === 'val') {
+    builder.setVal(valueId, json ? api.builder.maybeConst(value) : api.builder.const(undefined));
+  }
+  return valueId;
 };
