1621451bec9088d0e36957d0e14a748581245d8e
streamich <streamich@gmail.com>
2023-11-28 00:51:33 +0100

    feat: 🎸 make str node value hoverable

--- src/ClickableJsonCrdt/nodes/JsonCrdtStrNode.tsx
+++ src/ClickableJsonCrdt/nodes/JsonCrdtStrNode.tsx
@@ -5,6 +5,22 @@ import {JsonCrdtProperty} from '../JsonCrdtProperty';
 import {JsonAtom} from '../../JsonAtom';
 import {useStyles} from '../../context/style';
 import type {StrNode} from 'json-joy/es2020/json-crdt';
+import {rule, theme} from 'nano-theme';
+import {useFocus} from '../../context/focus';
+import {id} from '../utils';
+
+const atomClass = rule({
+  d: 'inline-block',
+  cur: 'default',
+  bdrad: '2px',
+});
+
+const atomFocusedClass = rule({
+  out: '1px dotted transparent',
+  '&:hover': {
+    out: `1px dotted ${theme.color.sem.blue[0]}`,
+  },
+});
 
 export interface JsonCrdtStrNodeProps {
   node: NodeRef<StrNode>;
@@ -12,11 +28,16 @@ export interface JsonCrdtStrNodeProps {
 
 export const JsonCrdtStrNode: React.FC<JsonCrdtStrNodeProps> = ({node}) => {
   const {formal} = useStyles();
+  const {focused} = useFocus();
+
+  const isFocused = focused === id(node);
 
   return (
     <JsonCrdtRegion node={node}>
       <JsonCrdtProperty node={node} />
-      <JsonAtom value={node.node.view()} />
+      <span className={atomClass + (isFocused ? atomFocusedClass : '')}>
+        <JsonAtom value={node.node.view()} />
+      </span>
       {!!formal && ','}
     </JsonCrdtRegion>
   );
