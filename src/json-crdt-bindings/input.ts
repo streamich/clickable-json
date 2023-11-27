9f542ff0a5dcda486113e775983a9e976701d406
streamich <streamich@gmail.com>
2023-11-27 22:01:00 +0100

    refactor: 💡 createe utils and types files

--- src/json-crdt-bindings/input.ts
+++ src/json-crdt-bindings/input.ts
@@ -1,30 +1,15 @@
-import type {Model, StrApi, ApiPath} from "json-joy/es2020/json-crdt";
-import {ITimestampStruct} from "json-joy/es2020/json-crdt-patch/clock";
 import diff from 'fast-diff';
 import {invokeFirstOnly} from "../utils/invokeFirstOnly";
 import {Selection} from "./Selection";
+import {applyChange, idToIndex, indexToId} from "./util";
+import {SimpleChange} from "./types";
+import type {Model, StrApi, ApiPath} from "json-joy/es2020/json-crdt";
 
 const enum DIFF_CHANGE_TYPE {
   DELETE = -1,
   EQUAL = 0,
   INSERT = 1,
 }
-
-type SimpleChange = [position: number, remove: number, insert: string];
-
-const applyChange = (str: string, [position, remove, insert]: SimpleChange): string =>
-  str.slice(0, position) + insert + str.slice(position + remove);
-
-const indexToId = (str: StrApi, index: number): ITimestampStruct | void => {
-  return str.node.find(index)
-};
-
-const idToIndex = (str: StrApi, id: ITimestampStruct): number => {
-  const chunk = str.node.findById(id);
-  if (!chunk) return -1;
-  const pos = str.node.pos(chunk);
-  return pos + (chunk.del ? 0 : (id.time - chunk.id.time));
-};
 
 export class JsonCrdtBinding {
   public static bind = (model: Model, path: ApiPath, input: HTMLInputElement, polling?: boolean): (() => void) => {
